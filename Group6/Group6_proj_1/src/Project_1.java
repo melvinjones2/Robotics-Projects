f3bea7338332f898b60c23fe96a34e717ef760fb
kpyang5 <88057097+Kophya@users.noreply.github.com>
2025-10-12 16:43:36 -0500

    Add initial implementation of Project_1 with motor imports

---
+++ Group6/Group6_proj_1/src/Project_1.java
@@ -0,0 +1,12 @@
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.*;
+
+
+public class Project_1 {
+
+	public static void main(String[] args) {
+		EV3Motor 
+	}
+
+}
